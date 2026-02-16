b59516ce4fff9904d6f06fe84d05fce1c34cef5e
fm-labs <flowmotionlabs@gmail.com>
2026-02-16 20:23:24 +0100

    updated build scripts for linux

--- build_desktop.sh
+++ build_desktop.sh
@@ -28,6 +28,14 @@ if [[ "$TARGET_TRIPLE" == *"apple-darwin"* ]]; then
     fi
     cp -f "$BIN_FILE" "./ui/src-tauri/binaries/geenii-srv-${TARGET_TRIPLE}"
 
+elif [[ "$TARGET_TRIPLE" == *"unknown-linux-gnu"* ]]; then
+    BIN_FILE="./dist/bin/geenii-srv"
+    if [[ ! -f "$BIN_FILE" ]]; then
+        echo "Binary not found at $BIN_FILE. Please run build_bin.sh first."
+        exit 1
+    fi
+    cp -f "$BIN_FILE" "./ui/src-tauri/binaries/geenii-srv-${TARGET_TRIPLE}"
+
 elif [[ "$TARGET_TRIPLE" == *"windows"* ]]; then
     BIN_FILE="./dist/bin/geenii-srv.exe"
     echo "Not implemented yet. Skipping."
