1fcc420be0c6c41606fce6e8b60b37c878889856
fm-labs <flowmotionlabs@gmail.com>
2026-02-17 22:04:00 +0100

    selective build args

--- build_desktop.sh
+++ build_desktop.sh
@@ -7,7 +7,7 @@ set -xe
 
 WD=$(pwd)
 SIDECAR_BIN_DIR="./ui/src-tauri/binaries"
-#TARGET_TRIPLE=$(rustc --print host-tuple)
+TARGET_TRIPLE=$(rustc --print host-tuple)
 
 # Path or content of your private key
 KEY_PATH="$HOME/.tauri/signing.key"
@@ -18,44 +18,25 @@ export BUILD_DIST_DIR="$SIDECAR_BIN_DIR"
 source ./build_bin.sh
 
 
-#echo "Copying sidecar binaries ..."
-## copy to src-tauri/binaries with target triple in name
-#mkdir -p ./ui/src-tauri/binaries
-#if [[ "$TARGET_TRIPLE" == *"apple-darwin"* ]]; then
-#    BIN_FILE="./dist/bin/geeniid"
-#    if [[ ! -f "$BIN_FILE" ]]; then
-#        echo "Binary not found at $BIN_FILE. Please run build_bin.sh first."
-#        exit 1
-#    fi
-#    cp -f "$BIN_FILE" "./ui/src-tauri/binaries/geeniid-${TARGET_TRIPLE}"
-#
-#elif [[ "$TARGET_TRIPLE" == *"unknown-linux-gnu"* ]]; then
-#    BIN_FILE="./dist/bin/geeniid"
-#    if [[ ! -f "$BIN_FILE" ]]; then
-#        echo "Binary not found at $BIN_FILE. Please run build_bin.sh first."
-#        exit 1
-#    fi
-#    cp -f "$BIN_FILE" "./ui/src-tauri/binaries/geeniid-${TARGET_TRIPLE}"
-#
-#elif [[ "$TARGET_TRIPLE" == *"windows"* ]]; then
-#    BIN_FILE="./dist/bin/geeniid.exe"
-#    echo "Not implemented yet. Skipping."
-#    exit 1
-#fi
-
 cd ./ui
-pnpm install --frozen-lockfile
-#if ! pnpm run build ; then
-#    echo "UI build failed. Exiting."
-#    cd ..
-#    exit 1
-#fi
+pnpm install --frozen-lockfile || exit 1
+
+BUILD_ARGS=""
+if [[ "$TARGET_TRIPLE" == *"aarch64-unknown-linux"* ]]; then
+    BUILD_ARGS="--bundle deb,rpm"
+
+elif [[ "$TARGET_TRIPLE" == *"darwin"* ]]; then
+    BUILD_ARGS="--bundle app,dmg"
+
+elif [[ "$TARGET_TRIPLE" == *"windows"* ]]; then
+    BUILD_ARGS="--bundle exe"
+fi
 
 set +x
 export TAURI_SIGNING_PRIVATE_KEY="$KEY_CONTENT"
 export TAURI_SIGNING_PRIVATE_KEY_PASSWORD=""
 set -x
-if ! pnpm tauri build ; then
+if ! pnpm tauri build "$BUILD_ARGS" ; then
     echo "Tauri build failed. Exiting."
     cd ..
     exit 1
