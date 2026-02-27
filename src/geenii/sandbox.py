fccf7e8395d6adf67e77730cb20b24063b9cba87
fm-labs <flowmotionlabs@gmail.com>
2026-02-27 12:53:56 +0100

    added script args

--- src/geenii/sandbox.py
+++ src/geenii/sandbox.py
@@ -27,15 +27,16 @@ def run_docker_subprocess(command: list[str], timeout: int = 30) -> tuple[int, s
         return -1, "", f"Error running command: {str(e)}"
 
 
-def run_docker_sandbox_python(base_dir: str, script_name: str = "main.py", timeout: int = 60,
-                              mounts: list[str] = None) -> tuple[int, str, str]:
+def run_docker_sandbox_python(base_dir: str, script_name: str = "main.py", script_args: list[str] = None,
+                              mounts: list[str] = None, timeout: int = 30,) -> tuple[int, str, str]:
     """
     Run a Python script in a Docker sandbox.
 
     :param base_dir: The base directory containing the script to run.
     :param script_name: The name of the Python script to run (default: "main.py").
-    :param timeout: The maximum time to allow the command to run in seconds (default: 30).
+    :param script_args: List of additional arguments to pass to the Python script (default: None).
     :param mounts: A list of additional mount points in the format "host_path:container_path" (default: None).
+    :param timeout: The maximum time to allow the command to run in seconds (default: 30).
     :return: A tuple containing the exit code, stdout, and stderr.
     """
 
@@ -43,10 +44,12 @@ def run_docker_sandbox_python(base_dir: str, script_name: str = "main.py", timeo
     if mounts:
         for mount in mounts:
             command.extend(["-v", mount])
-    command.extend(["-v", f"{base_dir}:/app"])
+    command.extend(["-v", f"{base_dir}:/app:ro"])
     command.extend(["-w", "/app"])
     command.append("python:3.13-slim")
     command.extend(["python3", script_name])
+    if script_args:
+        command.extend(script_args)
 
     return run_docker_subprocess(command, timeout)
 
