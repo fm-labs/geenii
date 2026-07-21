6a764cd3f59b6eb505e8dc03bc33ab010d37a248
fm-labs <flowmotionlabs@gmail.com>
2026-07-21 09:37:30 +0200

    fixes

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 
 docker run --rm -it \
   --env-file=.env.docker \
-  -e OLLAMA_HOST=host.docker.internal:11434
+  -e OLLAMA_HOST=host.docker.internal:11434 \
   --add-host=host.docker.internal:host-gateway \
-  geenii:latest agent "Whats uuup?"
+  geenii:latest \
+  $@
